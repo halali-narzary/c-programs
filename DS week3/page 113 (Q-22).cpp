2118845c2abb14c1e5b0e4ee33cb2cad4b459343
halali-narzary <narzaryhalali@gmail.com>
2025-09-07 18:38:57 +0530

    Add files via upload

---
+++ DS week3/page 113 (Q-22).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int (*ptr)[3];
+    ptr = arr;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", *(*(ptr + i) + j));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
